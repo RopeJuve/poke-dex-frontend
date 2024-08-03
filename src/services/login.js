c119907bbf8b6d13740236e29d9a4df8470aa182
RopeJuve <rope.s@hotmail.com>
2024-08-03 16:08:15 +0200

    First init

--- src/services/login.js
+++ src/services/login.js
@@ -3,7 +3,7 @@ import { postData, fetchData } from "./fetchData";
 export const loginWithEmail = async (formData, login, navigate) => {
   try {
     const response = await postData(
-      "users-login-tan.vercel.app/api/connect",
+      "https://users-login-tan.vercel.app/api/connect",
       formData
     );
     const authHeader = response.headers.get("authorization");
@@ -20,7 +20,7 @@ export const loginWithEmail = async (formData, login, navigate) => {
 
 export const loginWithGoogle = async (login, navigate) => {
   try {
-    const response = await fetchData("users-login-tan.vercel.app/api/google");
+    const response = await fetchData("https://users-login-tan.vercel.app/api/google");
     const authHeader = response.headers.get("authorization");
     const token = authHeader.split(" ")[1];
     if (!token) {
