08af06f4adc9ccd28c288bd21f20cb6ee82e80ae
RopeJuve <rope.s@hotmail.com>
2024-08-03 16:14:56 +0200

    First init

--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -14,7 +14,7 @@ export default function PrivateRoute({ component: Component, ...rest }){
       const fetchUserData = async () => {
         try {
           const response = await axios.get(
-            'users-login-tan.vercel.app/api/auth/user',
+            'https://users-login-tan.vercel.app/api/auth/user',
             {
               headers: {
                 Authorization: `Bearer ${authToken}`,
